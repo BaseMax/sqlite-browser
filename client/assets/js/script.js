a18ac9ab43abcbd7d6695a6614ed80ebeaca01c2
ALI Mohammadiyeh (Max Base) <MaxBaseCode@gmail.com>
2025-01-05 17:56:44 +0100

    clean js

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -1,17 +1,63 @@
+// Elements
+const errorElm = document.getElementById('error');
 const execBtn = document.getElementById("execute");
 const outputElm = document.getElementById('output');
-const errorElm = document.getElementById('error');
-const commandsElm = document.getElementById('commands');
 const dbFileElm = document.getElementById('dbfile');
 const savedbElm = document.getElementById('savedb');
+const commandsElm = document.getElementById('commands');
 const tablesListElm = document.getElementById('tables-list');
 const editorContainerElm = document.getElementById('editor-container');
 
-const INITIAL_SQL_QUERY = "SELECT `name`, `sql`\n  FROM `sqlite_master`\n  WHERE type='table';";
-const worker = new Worker("assets/js/worker.sql-wasm.js");
-worker.onerror = error;
+// Variables
+let tictime;
 
-worker.postMessage({ action: 'open' });
+// Pre-load
+if (!window.performance || !performance.now) {
+    window.performance = { now: Date.now };
+}
+
+// Const variables
+const INITIAL_SQL_QUERY = "SELECT `name`, `sql`\n  FROM `sqlite_master`\n  WHERE type='table';";
+const editor = CodeMirror.fromTextArea(commandsElm, {
+    mode: 'text/x-mysql',
+    viewportMargin: Infinity,
+    indentWithTabs: true,
+    smartIndent: true,
+    lineNumbers: true,
+    matchBrackets: true,
+    autofocus: true,
+    extraKeys: {
+        "Ctrl-Enter": execEditorContents,
+        "Ctrl-S": savedb,
+    }
+});
+const tableCreate = function () {
+    function valconcat(vals, tagName) {
+        if (vals.length === 0) return '';
+        var open = '<' + tagName + '>', close = '</' + tagName + '>';
+        return open + vals.join(close + open) + close;
+    }
+
+    return function (columns, values) {
+        var tbl = document.createElement('table');
+        var html = '<thead>' + valconcat(columns, 'th') + '</thead>';
+        var rows = values.map(function (v) { return valconcat(v, 'td'); });
+        html += '<tbody>' + valconcat(rows, 'tr') + '</tbody>';
+        tbl.innerHTML = html;
+        return tbl;
+    };
+}();
+
+// Functions
+function tic() {
+    tictime = performance.now();
+}
+
+function toc(msg) {
+    const dt = performance.now() - tictime;
+
+    console.log((msg || 'toc') + ": " + dt + "ms");
+}
 
 function print(text) {
     outputElm.innerHTML = text.replace(/\n/g, '<br>');
@@ -54,22 +100,6 @@ function execute(commands) {
     outputElm.textContent = "Fetching results...";
 }
 
-const tableCreate = function () {
-    function valconcat(vals, tagName) {
-        if (vals.length === 0) return '';
-        var open = '<' + tagName + '>', close = '</' + tagName + '>';
-        return open + vals.join(close + open) + close;
-    }
-    return function (columns, values) {
-        var tbl = document.createElement('table');
-        var html = '<thead>' + valconcat(columns, 'th') + '</thead>';
-        var rows = values.map(function (v) { return valconcat(v, 'td'); });
-        html += '<tbody>' + valconcat(rows, 'tr') + '</tbody>';
-        tbl.innerHTML = html;
-        return tbl;
-    };
-}();
-
 function execEditorContents() {
     noerror();
     const editorValue = editor.getValue().trimEnd();
@@ -81,60 +111,6 @@ function execEditorContents() {
 
     execute(editorValue.endsWith(';') ? editorValue : editorValue + ';');
 }
-execBtn.addEventListener("click", execEditorContents, true);
-
-var tictime;
-if (!window.performance || !performance.now) { window.performance = { now: Date.now } }
-function tic() { tictime = performance.now() }
-function toc(msg) {
-    var dt = performance.now() - tictime;
-    console.log((msg || 'toc') + ": " + dt + "ms");
-}
-
-let editor = undefined;
-
-dbFileElm.onchange = function () {
-    var f = dbFileElm.files[0];
-    if (f) {
-        var r = new FileReader();
-        r.onload = function () {
-            console.log('File loaded:', f.name);
-            worker.onmessage = function (e) {
-                console.log('Worker response:', e.data); 
-
-                editor = CodeMirror.fromTextArea(commandsElm, {
-                    mode: 'text/x-mysql',
-                    viewportMargin: Infinity,
-                    indentWithTabs: true,
-                    smartIndent: true,
-                    lineNumbers: true,
-                    matchBrackets: true,
-                    autofocus: true,
-                    extraKeys: {
-                        "Ctrl-Enter": execEditorContents,
-                        "Ctrl-S": savedb,
-                    }
-                });
-                editorContainerElm.classList.remove('hidden');
-                execBtn.classList.remove('hidden');
-                savedbElm.classList.remove('hidden');
-
-                toc("Loading database from file");
-                editor.setValue(INITIAL_SQL_QUERY);
-                execEditorContents();
-            };
-            tic();
-            try {
-                worker.postMessage({ action: 'open', buffer: r.result }, [r.result]);
-            } catch (exception) {
-                worker.postMessage({ action: 'open', buffer: r.result });
-            }
-        };
-        r.readAsArrayBuffer(f);
-    } else {
-        error({ message: "No file selected" });
-    }
-};
 
 function populateTableList(tables) {
     tablesListElm.innerHTML = '';
@@ -150,24 +126,11 @@ function populateTableList(tables) {
         tablesListElm.appendChild(li);
     });
 }
-
-worker.onmessage = function (event) {
-    console.log("worker onmessage");
-    var data = event.data;
-    console.log(data, data.sql);
-    if (data.action === 'exec' && data.sql === "SELECT name FROM sqlite_master WHERE type='table';") {
-        console.log('Table names:', data.results);
-        populateTableList(data.results);
-    }
-    if (data.error) {
-        error({ message: data.error });
-    }
-};
-
 function savedb() {
     worker.onmessage = function (event) {
         toc("Exporting the database");
         const arraybuff = event.data.buffer;
+
         if (arraybuff.byteLength === 0) {
             alert("Database is empty, not saving");
             return;
@@ -183,10 +146,68 @@ function savedb() {
                 window.URL.revokeObjectURL(a.href);
             }, 1500);
         };
+
         a.click();
     };
+
     tic();
+
     worker.postMessage({ action: 'export' });
 }
 
+// Events
+execBtn.addEventListener("click", execEditorContents, true);
+
+dbFileElm.onchange = function () {
+    var f = dbFileElm.files[0];
+
+    if (f) {
+        const r = new FileReader();
+        r.onload = function () {
+            console.log('File loaded:', f.name);
+            worker.onmessage = function (e) {
+                console.log('Worker response:', e.data); 
+
+                editorContainerElm.classList.remove('hidden');
+                execBtn.classList.remove('hidden');
+                savedbElm.classList.remove('hidden');
+
+                toc("Loading database from file");
+                editor.setValue(INITIAL_SQL_QUERY);
+                execEditorContents();
+            };
+
+            tic();
+
+            try {
+                worker.postMessage({ action: 'open', buffer: r.result }, [r.result]);
+            } catch (exception) {
+                worker.postMessage({ action: 'open', buffer: r.result });
+            }
+        };
+
+        r.readAsArrayBuffer(f);
+    } else {
+        error({ message: "No file selected" });
+    }
+};
+
 savedbElm.addEventListener("click", savedb, true);
+
+
+// Worker
+const worker = new Worker("assets/js/worker.sql-wasm.js");
+worker.onerror = error;
+worker.postMessage({ action: 'open' });
+worker.onmessage = function (event) {
+    console.log("worker onmessage");
+    var data = event.data;
+    console.log(data, data.sql);
+    if (data.action === 'exec' && data.sql === "SELECT name FROM sqlite_master WHERE type='table';") {
+        console.log('Table names:', data.results);
+        populateTableList(data.results);
+    }
+    if (data.error) {
+        error({ message: data.error });
+    }
+};
